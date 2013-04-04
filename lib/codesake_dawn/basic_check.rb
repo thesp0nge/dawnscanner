762f89e3a30d1c03c344888742c7ed99868a73e2
Paolo Perego <thesp0nge@gmail.com>
2013-04-04 18:56:00 +0200

    Codesake::Dawn::BasicCheck is now able to detect if the MVC framework version is vulnerable or not

--- lib/codesake_dawn/basic_check.rb
+++ lib/codesake_dawn/basic_check.rb
@@ -19,6 +19,27 @@ module Codesake
         false
       end
 
+
+      # @target_version = '2.3.11'
+      # @fixes_version = ['2.3.18', '3.2.13', '3.1.12' ] 
+      def is_vulnerable_version?(target = nil, fixes = nil)
+        target  = @target_version if target.nil?
+        fixes   = @fixes_version  if fixes.nil?
+        return false if target.nil? or fixes.empty?
+
+        ret = false
+
+        target_v_array = target.split(".").map! { |n| n.to_i }
+        fixes.each do |fv|
+          fixes_v_array = fv.split(".").map! { |n| n.to_i }
+          if target_v_array[0] == fixes_v_array[0]
+            ret = true if target_v_array[1] < fixes_v_array[1] # same major but previous minor
+            ret = true if target_v_array[1] == fixes_v_array[1] and target_v_array[2] < fixes_v_array[2] 
+          end
+        end
+
+        ret
+      end
     end
   end
 end
