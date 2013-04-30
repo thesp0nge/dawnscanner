f0cac5807f07fddfbfd7284b0c263438b3c6e22f
Paolo Perego <thesp0nge@gmail.com>
2013-04-30 08:09:45 +0200

    Added test for CVE-2013-0333 that is the next to be queued in KB

--- spec/lib/dawn/codesake_knowledgebase_spec.rb
+++ spec/lib/dawn/codesake_knowledgebase_spec.rb
@@ -14,12 +14,7 @@ describe "The Codesake Dawn knowledge base" do
     kb.should   respond_to(:all_by_mvc)
   end
 
-  it "will find an existing security check by its name" do
-    sc = kb.find("CVE-2013-1855")
-    sc.should_not   be_nil
-    sc.class.should == Codesake::Dawn::Kb::CVE_2013_1855
-  end 
-  
+    
   it "will return a nil object if it doesn't find a particular security check"  do
     kb.find("A non existant security check name").should    be_nil
   end
@@ -28,7 +23,19 @@ describe "The Codesake Dawn knowledge base" do
     kb.all_by_mvc("sinatra").size.should   > 0
   end
 
-  it "must have test for CVE_2013_0333"
+  # KB Content
+  it "must have test for CVE_2013_1855" do
+    sc = kb.find("CVE-2013-1855")
+    sc.should_not   be_nil
+    sc.class.should == Codesake::Dawn::Kb::CVE_2013_1855
+  end 
+
+
+  it "must have test for CVE_2013_0333" do 
+    sc = kb.find("CVE-2013-0333")
+    sc.should_not   be_nil
+    sc.class.should == Codesake::Dawn::Kb::CVE_2013_0333
+  end
   it "must have test for CVE_2013_0269"
   it "must have test for CVE_2013_1857"
   it "must have test for CVE_2012_2660"
