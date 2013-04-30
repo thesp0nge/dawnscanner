00a2a5f8150e16598e6db2d0b8427d3a0915023a
Paolo Perego <thesp0nge@gmail.com>
2013-04-30 08:14:07 +0200

    Adding skeleton files for CVE-2013-0333

---
+++ lib/codesake/dawn/kb/cve_2013_0333.rb
@@ -0,0 +1,18 @@
+module Codesake
+	module Dawn
+		module Kb
+			# Automatically created with rake on 2013-04-30
+			class CVE_2013_0333
+				#
+				# Include the testing skeleton for this CVE
+				# include PatternMatchCheck
+				# include DependencyCheck
+				#
+
+
+				def initialize
+				end
+			end
+		end
+	end
+end
