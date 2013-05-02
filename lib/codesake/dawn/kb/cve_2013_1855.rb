b6dd73afb75162d6e1d090af0e279c5c43115469
Paolo Perego <thesp0nge@gmail.com>
2013-05-02 08:15:23 +0200

    Added auxiliary link

--- lib/codesake/dawn/kb/cve_2013_1855.rb
+++ lib/codesake/dawn/kb/cve_2013_1855.rb
@@ -5,11 +5,14 @@ module Codesake
         include PatternMatchCheck
 
         def initialize
-          super({:fixes_version => ['2.3.18', '3.2.13', '3.1.12'], 
-                :attack_pattern => ["sanitize_css"], 
-                :name=>'CVE-2013-1855', 
-                :applies=>["rails"],
-                :kind => Codesake::Dawn::KnowledgeBase::PATTERN_MATCH_CHECK})
+          super({
+            :fixes_version => ['2.3.18', '3.2.13', '3.1.12'], 
+            :attack_pattern => ["sanitize_css"], 
+            :name=>'CVE-2013-1855', 
+            :applies=>["rails"],
+            :kind => Codesake::Dawn::KnowledgeBase::PATTERN_MATCH_CHECK,
+            :aux_links => ["https://groups.google.com/d/msg/rubyonrails-security/4_QHo4BqnN8/_RrdfKk12I4J"]
+          })
         end
       end
     end
