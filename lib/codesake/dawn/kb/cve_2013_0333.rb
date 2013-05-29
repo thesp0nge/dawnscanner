aa2ec4e8f4a4b0f83e3969b0064d8735bc5a8834
Paolo Perego <thesp0nge@gmail.com>
2013-05-29 09:44:40 +0200

    Removed useless comments

--- lib/codesake/dawn/kb/cve_2013_0333.rb
+++ lib/codesake/dawn/kb/cve_2013_0333.rb
@@ -3,9 +3,6 @@ module Codesake
 		module Kb
 			# Automatically created with rake on 2013-04-30
 			class CVE_2013_0333
-				#
-				# Include the testing skeleton for this CVE
-				# include PatternMatchCheck
 				include DependencyCheck
 
 
