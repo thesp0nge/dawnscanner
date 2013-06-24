744eb17baa152c0bb0e12f2cfe7a9f944e4e3986
Paolo Perego <thesp0nge@gmail.com>
2013-06-24 15:45:18 +0200

    Fixing issue: #13 Could not locate Gemfile (Bundler::GemfileNotFound) in MVC detection

--- lib/codesake/dawn/core.rb
+++ lib/codesake/dawn/core.rb
@@ -2,10 +2,12 @@ module Codesake
   module Dawn
     class Core
       def self.detect_mvc(target)
-        gemfile_lock =  File.join(target, "Gemfile.lock")
-        raise ArgumentError.new("no Gemfile.lock in #{target}") unless File.exist?(gemfile_lock)
+        my_dir = Dir.pwd
+        Dir.chdir(target) 
+        raise ArgumentError.new("no Gemfile.lock in #{target}") unless File.exist?("Gemfile.lock")
 
-        lockfile = Bundler::LockfileParser.new(Bundler.read_file(gemfile_lock))
+        lockfile = Bundler::LockfileParser.new(Bundler.read_file("Gemfile.lock"))
+        Dir.chdir(my_dir)
         lockfile.specs.each do |s|
           return Codesake::Dawn::Rails.new(target)    if s.name == "rails"
           # return Codesake::Dawn::Padrino.new  if s.name == "padrino"
