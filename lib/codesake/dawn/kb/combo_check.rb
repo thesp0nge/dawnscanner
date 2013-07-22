326de5b47865dd57a24a2d7114d320fab19759c2
Paolo Perego <thesp0nge@gmail.com>
2013-07-22 18:58:55 +0200

    Added an option telling the combo check to marked as vulnerable even if only a signle check in the combo is vuln?

--- lib/codesake/dawn/kb/combo_check.rb
+++ lib/codesake/dawn/kb/combo_check.rb
@@ -11,12 +11,15 @@ module Codesake
 
         def initialize(options={})
           super(options)
+          @vuln_if_all_fails = true
+          @vuln_if_all_fails = options[:vuln_if_all_fails] unless options[:checks].nil?
           @checks = options[:checks]
           @vulnerable_checks = []
         end
 
         def vuln?
           ret = true
+          at_least_one = false
           @checks.each do |check|
             check_vuln = false
             check.detected_ruby   = @options[:detected_ruby]    if check.kind == Codesake::Dawn::KnowledgeBase::RUBY_VERSION_CHECK
@@ -26,12 +29,31 @@ module Codesake
             check_vuln = check.vuln? if check.respond_to?(:vuln?)
 
             ret = ret && check_vuln
+            at_least_one = true if check_vuln
             @evidences << check.evidences if check_vuln 
             @vulnerable_checks << check if check_vuln
             raise "A check class doesn't respond to vuln? in combo (#{check.class})" unless check.respond_to?(:vuln?)
           end
 
-          ret
+          dump_status
+          $logger.log("V: #{@vuln_if_all_fails}, R: #{ret}, A:#{at_least_one}")
+          return ret if @vuln_if_all_fails
+          return at_least_one unless @vuln_if_all_fails
+        end
+
+        def dump_status
+          @checks.each do |check|
+            $logger.log("#{check.name}: #{check.status}")
+          end
+
+          true
+        end
+
+        def self.find_vulnerable_checks_by_class(list=[], klass=Object)
+          list.each do |l|
+            return l if l.instance_of?(klass)
+          end
+          nil
         end
 
       end
