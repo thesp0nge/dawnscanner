a3996c71355e764ffa25448a058015c1bc084a42
Paolo Perego <thesp0nge@gmail.com>
2013-10-30 11:40:06 +0100

    Fixing an encoding ISSUE

--- lib/codesake/dawn/kb/owasp_ror_cheatsheet/command_injection.rb
+++ lib/codesake/dawn/kb/owasp_ror_cheatsheet/command_injection.rb
@@ -6,7 +6,7 @@ module Codesake
           include PatternMatchCheck
 
           def initialize
-            message = "Ruby offers a function called “eval” which will dynamically build new Ruby code based on Strings. It also has a number of ways to call system commands. While the power of these commands is quite useful, extreme care should be taken when using them in a Rails based application. Usually, its just a bad idea. If need be, a whitelist of possible values should be used and any input should be validated as thoroughly as possible. The Ruby Security Reviewer's Guide has a section on injection and there are a number of OWASP references for it, starting at the top: Command Injection."
+            message = "Ruby offers a function called \"eval\" which will dynamically build new Ruby code based on Strings. It also has a number of ways to call system commands. While the power of these commands is quite useful, extreme care should be taken when using them in a Rails based application. Usually, its just a bad idea. If need be, a whitelist of possible values should be used and any input should be validated as thoroughly as possible. The Ruby Security Reviewer's Guide has a section on injection and there are a number of OWASP references for it, starting at the top: Command Injection."
 
             super({
               :name=>"Owasp Ror CheatSheet: Command Injection",
