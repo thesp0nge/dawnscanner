c3909adad53af2b60b8ab73ccea2f39a400b1b68
Paolo Perego <thesp0nge@gmail.com>
2014-01-08 08:18:45 +0100

    Adding missing files... oops :-)

---
+++ spec/lib/kb/cve_2011_3009_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+describe "The CVE-2011-3009 vulnerability" do
+	before(:all) do
+		@check = Codesake::Dawn::Kb::CVE_2011_3009.new
+		# @check.debug = true
+	end
+	it "needs some test..."
+end
