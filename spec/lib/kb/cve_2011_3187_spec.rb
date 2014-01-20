f3be8fcf267469e7f51d77616ef768af372171eb
Paolo Perego <thesp0nge@gmail.com>
2014-01-20 18:20:10 +0100

    Added spec

--- spec/lib/kb/cve_2011_3187_spec.rb
+++ spec/lib/kb/cve_2011_3187_spec.rb
@@ -4,5 +4,21 @@ describe "The CVE-2011-3187 vulnerability" do
 		@check = Codesake::Dawn::Kb::CVE_2011_3187.new
 		# @check.debug = true
 	end
-	it "needs some test..."
+  it "fires when vulnerable rails version it has been found (3.0.5)" do
+    @check.dependencies = [{:name=>'rails', :version=>'3.0.5'}]
+    @check.vuln?.should be_true
+  end
+  it "doesn't fire when safe rails version it has been found (3.0.6)" do
+    @check.dependencies = [{:name=>'rails', :version=>'3.0.6'}]
+    @check.vuln?.should be_false
+  end
+  it "doesn't fire when safe rails version it has been found (3.1.6)" do
+    @check.dependencies = [{:name=>'rails', :version=>'3.1.6'}]
+    @check.vuln?.should be_false
+  end
+  it "doesn't fire when safe rails version it has been found (2.3.16)" do
+    @check.dependencies = [{:name=>'rails', :version=>'2.3.16'}]
+    @check.vuln?.should be_false
+  end
+          # self.safe_dependencies = [{:name=>"rails", :version=>['3.0.6']}]
 end
