59d98d5574e96ef4c8ccc803daec37f5c745c4e1
Paolo Perego <thesp0nge@gmail.com>
2014-02-13 11:24:02 +0100

    Added Rails Railitie

--- lib/codesake/dawn/tasks.rb
+++ lib/codesake/dawn/tasks.rb
@@ -1,5 +1,27 @@
 require 'rake'
-desc 'Execute codesake-dawn on the current directory'
-task :dawn do
-  system "dawn ."
+
+namespace :dawn do
+
+  desc 'Execute codesake-dawn on the current directory'
+  task :require do
+    begin
+      require 'codesake-dawn'
+    rescue LoadError => e
+      if defined?(Rails)
+        Rake::Task['dawn:require_environment'].invoke
+      else
+        raise e
+      end
+    end
+  end
+  task :require_environment do
+    if defined?(Rails)
+      Rake::Task['environment'].invoke
+    end
+    require 'codesake-dawn'
+  end
+
+  task :run do
+    system "dawn ."
+  end
 end
