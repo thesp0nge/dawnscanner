1e825bc21746ab9d8f5b931cef8fae4fddc2b5db
Paolo Perego <thesp0nge@gmail.com>
2014-03-14 12:45:27 +0100

    Removing UTF-8

--- lib/codesake/dawn/kb/owasp_ror_cheatsheet/check_for_safe_redirect_and_forward.rb
+++ lib/codesake/dawn/kb/owasp_ror_cheatsheet/check_for_safe_redirect_and_forward.rb
@@ -7,14 +7,26 @@ module Codesake
           include PatternMatchCheck
 
           def initialize
-            message = <<EOT
-          Web applications often require the ability to dynamically redirect users based on client-supplied data. To clarify, dynamic redirection usually entails the client including a URL in a parameter within a request to the application. Once received by the application, the user is redirected to the URL specified in the request. For example:
-            http://www.example.com/redirect?url=http://www.example_commerce_site.com/checkout
-          The above request would redirect the user to http://www.example.com/checkout. The security concern associated with this functionality is leveraging an organization’s trusted brand to phish users and trick them into visiting a malicious site, in our example, “badhacker.com”. Example:
-            http://www.example.com/redirect?url=http://badhacker.com
-          The most basic, but restrictive protection is to use the :only_path option. Setting this to true will essentially strip out any host information.
+            message = <<-EOT
+Web applications often require the ability to dynamically redirect users based
+on client-supplied data. To clarify, dynamic redirection usually entails the
+client including a URL in a parameter within a request to the application. Once
+received by the application, the user is redirected to the URL specified in the
+request.
 
-EOT
+For example: http://www.example.com/redirect?url=http://www.example_commerce_site.com/checkout
+
+The above request would redirect the user to http://www.example.com/checkout.
+
+The security concern associated with this functionality is leveraging an
+organization's trusted brand to phish users and trick them into visiting a
+malicious site, in our example, "badhacker.com".
+
+Example: http://www.example.com/redirect?url=http://badhacker.com
+
+The most basic, but restrictive protection is to use the :only_path option.
+Setting this to true will essentially strip out any host information.
+            EOT
 
             super({
               :name=>"Owasp Ror CheatSheet: Check for safe redirect and forward",
