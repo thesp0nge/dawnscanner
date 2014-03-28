482a3c1668cc8c8c54451a4d67c01a1ba112f3f7
Paolo Perego <thesp0nge@gmail.com>
2014-03-28 12:09:12 +0100

    Nokogiri XML Dos now has its own CVE: CVE-2013-6460

--- spec/lib/dawn/codesake_knowledgebase_spec.rb
+++ spec/lib/dawn/codesake_knowledgebase_spec.rb
@@ -427,10 +427,10 @@ describe "The Codesake Dawn knowledge base" do
     sc.class.should == Codesake::Dawn::Kb::CVE_2013_6417
   end
 
-  it "must have test for NokogiriDos20131217_1" do
-    sc = kb.find("Nokogiri - Denial of service - 20131217")
+  it "must have test for CVE-2013-6460" do
+    sc = kb.find("CVE-2013-6460")
     sc.should_not   be_nil
-    sc.class.should == Codesake::Dawn::Kb::NokogiriDos20131217
+    sc.class.should == Codesake::Dawn::Kb::CVE_2013_6460
   end
   it "must have test for Nokogiri_EntityExpansion_Dos_20131217" do
     sc = kb.find("Nokogiri - Entity expasion denial of service - 20131217")
