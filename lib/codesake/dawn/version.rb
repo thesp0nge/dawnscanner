e33cd5de5f704b84b271c254a86e646552b2c1a0
Paolo Perego <thesp0nge@gmail.com>
2014-05-06 22:28:53 +0200

    Patch number bump

--- lib/codesake/dawn/version.rb
+++ lib/codesake/dawn/version.rb
@@ -19,10 +19,10 @@ module Codesake
     # |   "Luigi"       |  7.0.0  |
     # | "Doc Hudson"    |  8.0.0  |
 
-    VERSION   = "1.1.2"
+    VERSION   = "1.1.3"
     CODENAME  = "Lightning McQueen"
     # RELEASE   = "(development)"
-    RELEASE   = "20140422"
+    RELEASE   = "20140506"
 
   end
 end
