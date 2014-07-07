4d1b37dab2eeb6975b0c91cafad9a9e4177309c0
Paolo Perego <thesp0nge@gmail.com>
2014-07-07 18:44:02 +0200

    Issue #75. Added auxiliary link

--- spec/lib/kb/cve_2014_3483_spec.rb
+++ spec/lib/kb/cve_2014_3483_spec.rb
@@ -5,7 +5,6 @@ describe "The CVE-2014-3483 vulnerability" do
 		# @check.debug = true
 	end
   it "must be filled with CVSS information"
-  it "must be filled with auxiliary link"
   it "is reported when a rails gem version 4.0.6 is detected" do
     @check.dependencies = [{:name=>"rails", :version=>"4.0.6"}]
     @check.vuln?.should   be_true
