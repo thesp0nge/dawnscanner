78053ab04e5e74de063d5ba4cc77103bbfacee7a
Abby Henríquez Tejera <paradoja@gmail.com>
2015-04-16 21:01:45 +0100

    Typo fix in session_stored_in_database.rb

--- lib/dawn/kb/owasp_ror_cheatsheet/session_stored_in_database.rb
+++ lib/dawn/kb/owasp_ror_cheatsheet/session_stored_in_database.rb
@@ -20,7 +20,7 @@
               :avoid_comments=>true,
               :check_family=>:owasp_ror_cheatsheet,
               :severity=>:info,
-              :evidences=>["In your session_store.rb file you are not using ActiveRercord to store session data. This will let rails to use a cookie based session and it can expose your web application to a session replay attack."],
+              :evidences=>["In your session_store.rb file you are not using ActiveRecord to store session data. This will let rails to use a cookie based session and it can expose your web application to a session replay attack."],
               :mitigation=>"Use ActiveRecord or the ORM you love most to handle your code session_store. Add \"Application.config.session_store :active_record_store\" to your session_store.rb file."
             })
             # @debug = true
