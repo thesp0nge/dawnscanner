54c62bc34fa66494997c20ee6711f9e640b96b8a
Paolo Perego <thesp0nge@gmail.com>
2015-09-16 18:28:43 +0200

    v 1.4.0 - Tow Mater

--- lib/dawn/version.rb
+++ lib/dawn/version.rb
@@ -1,7 +1,7 @@
 module Dawn
-    VERSION = "1.3.5"
-    CODENAME = "Lightning McQueen"
-    RELEASE = "20150729"
-    BUILD = "31"
-    COMMIT = "ge32c804"
+    VERSION = "1.4.0"
+    CODENAME = "Tow Mater"
+    RELEASE = "20150916"
+    BUILD = "21"
+    COMMIT = "gb184185"
 end
