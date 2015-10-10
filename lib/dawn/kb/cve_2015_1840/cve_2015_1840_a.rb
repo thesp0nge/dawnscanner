bed6695c8b3d7fc946baae9fe31374604e34abd0
Walter Mottinelli <wmotti@gmail.com>
2015-10-10 23:40:30 +0200

    Fix name for CVE-2015-1840

--- lib/dawn/kb/cve_2015_1840/cve_2015_1840_a.rb
+++ lib/dawn/kb/cve_2015_1840/cve_2015_1840_a.rb
@@ -8,7 +8,7 @@ module Dawn
           message = "jquery_ujs.js in jquery-rails before 3.1.3 and 4.x before 4.0.4 and rails.js in jquery-ujs before 1.0.4, as used with Ruby on Rails 3.x and 4.x, allow remote attackers to bypass the Same Origin Policy, and trigger transmission of a CSRF token to a different-domain web server, via a leading space character in a URL within an attribute value."
 
           super({
-            :name=>"CVE-2015-1849",
+            :name=>"CVE-2015-1840",
             :cvss=>"AV:N/AC:L/Au:N/C:P/I:N/A:N",
             :release_date => Date.new(2015, 7, 26),
             :cwe=>"200",
