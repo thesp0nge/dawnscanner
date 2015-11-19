a8fdb25291e8aabcb8d54c8fc52697bc219c5f6f
Paolo Perego <thesp0nge@gmail.com>
2015-11-19 08:23:48 +0100

    Typos

--- lib/dawn/utils.rb
+++ lib/dawn/utils.rb
@@ -1,5 +1,8 @@
 module Dawn
   module Utils
+    def debug_me(msg)
+      $logger.debug(msg) if @debug
+    end
 
     def debug_me_and_return_true(msg)
       __debug_me_and_return(msg, true)
@@ -7,10 +10,6 @@ module Dawn
     def debug_me_and_return_false(msg)
       __debug_me_and_return(msg, false)
     end
-    def debug_me(msg)
-      $logger.debug(msg) if @debug
-    end
-
     def __debug_me_and_return(msg, status)
       $logger.debug(msg) if @debug
       return status
