766b93b729aa7fe06a5a3c3e953f2bbc183df3fd
Paolo Perego <thesp0nge@gmail.com>
2015-12-02 22:34:09 +0100

    Issue #124 - Adding a check for OSVDB 115654 : Sentry raven-ruby lib/raven/okjson.rb Exponent / Scientific Notation Value Handling Resource Consumption DoS

--- spec/lib/dawn/codesake_knowledgebase_spec.rb
+++ spec/lib/dawn/codesake_knowledgebase_spec.rb
@@ -1059,4 +1059,9 @@ describe "The Codesake Dawn knowledge base" do
     sc.should_not   be_nil
     sc.class.should == Dawn::Kb::OSVDB_117903
   end
+  it "must have test for OSVDB_115654" do
+    sc = kb.find("OSVDB_115654")
+    sc.should_not   be_nil
+    sc.class.should == Dawn::Kb::OSVDB_115654
+  end
 end
