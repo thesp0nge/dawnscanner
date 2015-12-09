1d43c6281090a04d8c078650cbb2d2e1ff5e75c1
Paolo Perego <thesp0nge@gmail.com>
2015-12-09 17:04:06 +0100

    v1.5.1

--- lib/dawn/version.rb
+++ lib/dawn/version.rb
@@ -1,7 +1,7 @@
 module Dawn
-    VERSION = "1.5.0"
+    VERSION = "1.5.1"
     CODENAME = "Tow Mater"
     RELEASE = "20151209"
-    BUILD = "60"
-    COMMIT = "g24e3b7e"
+    BUILD = "4"
+    COMMIT = "g4ec31ef"
 end
