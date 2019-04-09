103afd3abde4ecfc6afa2f5372362d2267647189
Perego Paolo <paolo.perego@gruppomol.it>
2019-04-09 14:29:30 +0200

    RC1

--- lib/dawn/version.rb
+++ lib/dawn/version.rb
@@ -1,7 +1,7 @@
 module Dawn
-    VERSION = "2.0.g653723d"
-    CODENAME = "Finn McMissile"
-    RELEASE = "(development)"
-    BUILD = "7"
-    COMMIT = "g653723d"
+  VERSION = "2.0.0.rc1"
+  CODENAME = "Finn McMissile"
+  RELEASE = "(development)"
+  BUILD = "7"
+  COMMIT = "g653723d"
 end
